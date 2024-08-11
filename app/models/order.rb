fa9681adc935e704d8c8a9665e8b3d57bc793ab1
nana <sss1230871997@gmail.com>
2024-08-11 14:19:38 +0900

    refile4

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
-  has_and_belongs_to_many :foods
   belongs_to :customer
   belongs_to :address
   has_many :order_foods
+  has_many :foods, through: :order_foods
 end
